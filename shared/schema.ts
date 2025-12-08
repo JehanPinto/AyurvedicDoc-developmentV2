9af6a54d0a9eb98370365e751de29fda9a47e9fa
Kumarage Don Punsara Ridma Jayawardena <ridmajayawardanarc@gmail.com>
2025-12-08 21:28:49 +0530

    new

--- shared/schema.ts
+++ shared/schema.ts
@@ -586,7 +586,7 @@ export interface Notification extends InsertNotification {
 export const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(1, "Password is required"),
-  rememberMe: z.boolean().optional(),
+
 });
 
 export type LoginInput = z.infer<typeof loginSchema>;
