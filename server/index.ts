bd2adaf8bd17ca993717c0c4ac2f28561ca77d46
Jehan Pinto <162337966+JehanPinto@users.noreply.github.com>
2026-05-14 21:04:54 +0530

    Refactor server port configuration to default to localhost

--- server/index.ts
+++ server/index.ts
@@ -96,15 +96,16 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  const host = process.env.HOST || "0.0.0.0";
-  httpServer.listen(
-    {
-      port,
-      host,
-    },
-    () => {
-      log(`serving on http://${host}:${port}`);
-    },
-  );
+const port = parseInt(process.env.PORT || "5000", 10);
+const host = process.env.HOST || "localhost";
+
+httpServer.listen(
+  {
+    port,
+    host,
+  },
+  () => {
+    log(`serving on http://${host}:${port}`);
+  },
+);
 })();
